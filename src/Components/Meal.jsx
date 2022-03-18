cb4a97c092d19680be705055353520c48f240d8d
Vhanne-Ranjeet <88040470+Vhanne-Ranjeet@users.noreply.github.com>
2022-03-18 16:56:24 +0530

    Initial commit

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Meal.css";
+
+function Meal(props) {
+  return (
+    <div
+      className="meal"
+      onClick={() => {
+        props.setDisplayMeal(props.meal);
+      }}
+    >
+      <img
+        className="meal-img"
+        src={props.meal.strMealThumb}
+        alt={props.meal.strMeal}
+      ></img>
+      <div className="hover-name">{props.meal.strMeal}</div>
+    </div>
+  );
+}
+
+export default React.memo(Meal);
